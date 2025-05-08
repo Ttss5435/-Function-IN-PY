f3adcd5b4bd508a72c04b1dd0c33d66710358b6f
Trisha Singh <trishasingh3131@gmail.com>
2025-05-08 11:02:48 +0530

    Add files via upload

---
+++ 1st Module.py.py
@@ -0,0 +1,20 @@
+# Function IN PY
+import math
+import File1
+# print (math.sqrt(9))
+# print (math.sqrt(144))
+# print (math.floor(1.2))
+# print(math.ceil(1.4))
+# x=math.pi
+# print(x)
+# print (File1.sum(8,5))
+# print (File1.sub(8,5))
+# a=int(input("Enter a first Number"))
+# b=int (input("Enter a second Number"))
+# print(File1.sum(a,b))
+# print(File1.sub(a,b))
+# print(File1.div(a,b))
+# print(File1.multiply(a,b))
+
+
+
